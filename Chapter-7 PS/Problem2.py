f24518e165afba421563d9e0f969d590d9ee4725
Anuj Chaudhary <alwaysmile9813@gmail.com>
2024-06-27 11:52:33 +0530

    updates

--- Chapter-7 PS/Problem2.py
+++
@@ -1,8 +0,0 @@
-# Program to greet all the person in the list whose name starts with A
-
-l = ['Anuj', 'Dev', 'Rahul', 'Arav', 'Ayan', 'Sobhit']
-
-for i in l:
-    if i.startswith('A'):
-        print(f'Hello, {i}')
-        
