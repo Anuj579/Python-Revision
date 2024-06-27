6e69a7645f66f2f086beee1e7ff40f644e9491d8
Anuj Chaudhary <alwaysmile9813@gmail.com>
2024-06-27 13:01:16 +0530

    ch-8 functions and recursions revision

---
+++ Chapter-8/01_functions.py
@@ -0,0 +1,28 @@
+# a = int(input("Enter a number: "))
+# b = int(input("Enter a number: "))
+# c = int(input("Enter a number: "))
+
+# avg = (a+b+c)/3
+# print("The average of the three numbers is: ", avg)
+
+# a = int(input("Enter a number: "))
+# b = int(input("Enter a number: "))
+# c = int(input("Enter a number: "))
+
+# avg = (a+b+c)/3
+# print("The average of the three numbers is: ", avg)   #Instead of this we use function if we want to repeat the task many times
+
+# Function Definition
+def avg():
+    a = int(input("Enter a number: "))
+    b = int(input("Enter a number: "))
+    c = int(input("Enter a number: "))
+
+    avg = (a+b+c)/3
+    print("The average of the three numbers is: ", avg)
+
+# Function call
+avg()
+avg()
+avg()
+avg()
