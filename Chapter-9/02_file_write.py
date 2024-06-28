70fac096d52da8f3015590b9a3f67d0faa36e8cc
Anuj Chaudhary <alwaysmile9813@gmail.com>
2024-06-28 23:06:12 +0530

    ch-9 file I/O revision

---
+++ Chapter-9/02_file_write.py
@@ -0,0 +1,6 @@
+str = 'Hi, I am Anuj. I am a Full stack Developer.'
+
+f= open('myFile.txt', 'w')
+
+f.write(str)
+f.close()
---
+++ Chapter-9/04_append.py
@@ -0,0 +1,6 @@
+str = 'Hi, I am Anuj. I am a Full stack Developer.'
+
+f= open('myFile.txt', 'a')
+
+f.write(str)
+f.close()
