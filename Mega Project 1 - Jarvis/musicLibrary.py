d815fd3bbb86808fd54f9ff6c166ab613d45cdd6
Anuj Chaudhary <alwaysmile9813@gmail.com>
2024-07-18 20:04:03 +0530

    Mega Project 1 - Jarvis

---
+++ Mega Project 1 - Jarvis/musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "husn": 'https://www.youtube.com/watch?v=0IIJxkDtkHY',
+    "baarish": 'https://www.youtube.com/watch?v=6fTilfKvxbo',
+    "night": 'https://www.youtube.com/watch?v=7KPyunRIjr0&ab_channel=AuraMelodies',
+
+}
